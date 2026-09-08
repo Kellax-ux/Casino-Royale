5541b42aea869da2b2449002111f1adbb00d305b
Kellax-ux <makkartanishq41@gmail.com>
2026-09-08 15:24:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-db = SQL("sqlite:///data/he.db")
+db = SQL("sqlite:////data/he.db")
 
 app.config["SESSION_PERMANENT"]= False
 app.config["SESSION_TYPE"]= "filesystem"
